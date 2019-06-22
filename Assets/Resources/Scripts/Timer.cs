1c3eacd187af8e6bbc2e23f0b38a0ac1e7940caf
Oswalman <oswaldo-007@hotmail.com>
2019-06-22 18:28:08 -0500

    Sound effects, Environment etc.

---
+++ Assets/Resources/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{
+    [Header("Texto:")]
+    public Text text;
+    
+    private float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - startTime;
+        string minutes = ((int)t / 60).ToString();
+        string second = (t % 60).ToString("f2");
+
+        text.text = minutes+":"+second;
+    }
+}
