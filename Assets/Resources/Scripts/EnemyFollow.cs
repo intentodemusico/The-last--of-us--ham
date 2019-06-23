13a6ce41cb71d3a9923ae766f93b79438eb3e10c
Oswalman <oswaldo-007@hotmail.com>
2019-06-22 23:19:35 -0500

    FollowZombieAI

---
+++ Assets/Resources/Scripts/EnemyFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    
+    public GameObject player;
+    NavMeshAgent enemy;
+    Animator enemy2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy=GetComponent<NavMeshAgent>();
+        enemy2 = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.destination = player.transform.position;
+        enemy2.SetFloat("Speed", Mathf.Abs(player.transform.position.x));
+    }
+}
