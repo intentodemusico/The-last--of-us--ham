043763e3e81c5f3811c43dce85718a683eb77eaf
Oswalman <oswaldo-007@hotmail.com>
2019-06-25 08:23:39 -0500

    Code, Bugs...

---
+++ Assets/GameOver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    AudioClip sonido;
+    AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            StartCoroutine(waiting());
+        }
+    }
+    private IEnumerator waiting()
+    {
+        sonido = (AudioClip)Resources.Load("Audio/GameOver", typeof(AudioClip));
+        sound.PlayOneShot(sonido, 2f);
+        yield return new WaitWhile(() => sound.isPlaying);
+        Scene escena = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(escena.name);
+
+        // Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
